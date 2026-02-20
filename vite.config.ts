7c83179bd7e8d71cb0d156632d776a29aee6d577
Guilherme <user@example.com>
2026-02-20 10:25:44 -0300

    Build: Add docs folder for GitHub Pages and update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,12 @@
-Not found
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindPlugin from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindPlugin()],
+  base: '/QuickChat.github.io/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
+})
